14dbf8e331f1f759fdb2cbbc870ae00d1842a106
Nate Schaffner <thx1766@gmail.com>
2022-11-15 08:56:32 -0500

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Nate M1 on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
